b8f26c3cabfd66c134c4e69bba2ee4ab25875391
woojinhong <orolzleim@gmail.com>
2026-04-13 15:18:52 +0900

    자동화 스크림트로 받아온 데이터셋 약 300개 파일 편집기로 추출

--- reacts/src/App.jsx
+++ reacts/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 22}>
+        <button onClick={() => setCount((count) => count + 222}>
           count is {count}
         </button>
         <p>
