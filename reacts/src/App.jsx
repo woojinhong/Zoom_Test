e262458298b1f45a2bf4fd9563b0e0ce26d7e17c
woojinhong <orolzleim@gmail.com>
2026-04-13 15:28:42 +0900

    새로운 내용 여기 붙어라 길이 모여라 모이를 먹어라 리리리리라ㅏ라라

--- reacts/src/App.jsx
+++ reacts/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 2222}>
+        <button onClick={() => setCount((count) => count + 21222}>
           count is {count}
         </button>
         <p>
